458420c6ffbab2db50538e3216b2f94cbd97584d
Ben Francis <ben3dprints1@gmail.com>
2026-03-06 15:25:51 -0600

    added images to array project

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,6 +5,14 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let symbols = ["bar", "bell", "cherries", "clover", "coin", "gem", "horseshoe", "seven"];
+
+function preload() {
+  for (let i = 0; i < symbols.length; i++) {
+    let symbol = symbols[i];
+    symbols[i] = loadImage(`images/${symbol}.png`);
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
